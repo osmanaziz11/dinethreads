8cede9c0eb1fdfc6305a9f6de1b6519041ef801e
Osman Aziz <osmanaziz012@gmail.com>
2025-10-11 16:11:07 +0500

    client section wrap line issue

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const Products = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-center">Products Page (TBD)</h1>
+      <h1 className="text-center">Products Page Coming Soon!</h1>
     </div>
   );
 };
